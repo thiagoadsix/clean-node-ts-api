05b7c7d2b6ea094b753057a6af59976de173af93
Thiago Andrade Silva <thiago.adsix@gmail.com>
2022-01-23 21:41:58 -0300

    test: ensure BCryptAdapter throw if BCrypt throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -25,4 +25,17 @@ describe('BCrypt Adapter', () => {
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash')
   })
+
+  test('should throw if BCrypt throws', async () => {
+    const sut = makeSut()
+
+    jest
+      .spyOn(bcrypt, 'hash')
+      .mockImplementationOnce(() => {
+        throw new Error()
+      })
+
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
+  })
 })
