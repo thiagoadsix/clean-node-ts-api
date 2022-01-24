8295316e920f74b1eea296ea7734122e8f12ed76
Thiago Andrade Silva <thiago.adsix@gmail.com>
2022-01-23 22:32:04 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account-mongo-repository.spec.ts
@@ -0,0 +1,27 @@
+import { MongoHelper } from '../../helpers/mongo-helper'
+import { AccountMongoRepository } from './account-mongo-repository'
+
+describe('Account MongoDB Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(String(process.env.MONGO_URL))
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'name',
+      email: 'email@example.com',
+      password: 'password'
+    })
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('name')
+    expect(account.email).toBe('email@example.com')
+    expect(account.password).toBe('password')
+  })
+})
