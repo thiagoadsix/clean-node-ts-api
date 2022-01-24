1bd2c6a2d44f392f36de3db7f8957960279f77e3
Thiago Andrade Silva <thiago.adsix@gmail.com>
2022-01-24 14:27:57 -0300

    test: ensure requests parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,19 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Body parser middleware', () => {
+  test('should parse body as json', async () => {
+    app.post('/test-body-parser', (req, res) => {
+      res.send(req.body)
+    })
+
+    await request(app)
+      .post('/test-body-parser')
+      .send({
+        name: 'Some Name'
+      })
+      .expect({
+        name: 'Some Name'
+      })
+  })
+})
