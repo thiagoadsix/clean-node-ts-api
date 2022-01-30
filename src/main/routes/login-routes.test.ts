c089ccdd11afb15d810f9ae097b921d4098bf9e0
Thiago Andrade Silva <thiago.adsix@gmail.com>
2022-01-30 18:29:30 -0300

    refactor: rename signuo routes with login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -4,7 +4,7 @@ import { MongoHelper } from '../../infra/db/helpers/mongo-helper'
 
 import app from '../config/app'
 
-describe('SignUp routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(String(process.env.MONGO_URL))
   })
@@ -18,15 +18,17 @@ describe('SignUp routes', () => {
     await accountCollection.deleteMany({})
   })
 
-  test('should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Some Name',
-        email: 'some@example.com',
-        password: 'somepassword',
-        passwordConfirmation: 'somepassword'
-      })
-      .expect(200)
+  describe('POST /signup', () => {
+    test('should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Some Name',
+          email: 'some@example.com',
+          password: 'somepassword',
+          passwordConfirmation: 'somepassword'
+        })
+        .expect(200)
+    })
   })
 })
