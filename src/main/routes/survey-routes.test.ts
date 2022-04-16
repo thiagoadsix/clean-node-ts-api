8041d24e7ea3211409fd25a4c01a8d7d06a9ffae
Thiago Andrade <thiago.adsix@gmail.com>
2022-04-16 14:51:59 -0300

    test: ensure add survey route return 204 on create a survey with valid token

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,11 +1,15 @@
+import { hash } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
 import { Collection } from 'mongodb'
 import request from 'supertest'
 
 import { MongoHelper } from '../../infra/db/helpers/mongo-helper'
 
 import app from '../config/app'
+import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -19,12 +23,35 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /surveys', () => {
-    test.skip('should return 204 on create a survey on success', async () => {
+    test('should return 204 on create a survey with valid token', async () => {
+      const password = await hash('123456789', 12)
+      const result = await accountCollection.insertOne({
+        name: 'Some Name',
+        email: 'some@example.com',
+        password,
+        role: 'admin'
+      })
+      const account = await accountCollection.findOne(result.insertedId)
+      const accountId = account._id
+      const accessToken = sign({ id: accountId }, env.jwtSecret)
+      await accountCollection.updateOne(
+        { _id: accountId }
+        ,
+        {
+          $set: {
+            accessToken
+          }
+        }
+      )
+
       await request(app)
         .post('/api/surveys')
+        .set('x-access-token', accessToken)
         .send({
           question: 'any question',
           answers: [{
