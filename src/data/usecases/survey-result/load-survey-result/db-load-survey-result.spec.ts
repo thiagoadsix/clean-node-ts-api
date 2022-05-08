d57047a8f8b43f30c07272ee86512a055824dc8c
Thiago Andrade <thiago.adsix@gmail.com>
2022-05-08 16:53:42 -0300

    test: ensure DbLoadSurveyResult throw if LoadSurveyResultRepository throws

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -1,3 +1,4 @@
+import { throwError } from '@/domain/test'
 import { mockLoadSurveyResultRepository } from '@/data/test'
 
 import { LoadSurveyResultRepository } from './db-load-survey-result-protocols'
@@ -24,5 +25,16 @@ describe('DbLoadSurveyResult Usecase', () => {
     const loadBySurveyIdSpy = jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId')
     await sut.load('any_survey_id')
     expect(loadBySurveyIdSpy).toHaveBeenCalledWith('any_survey_id')
+  })
+
+  test('should throw if LoadSurveyResultRepository throws', async () => {
+    const { sut, loadSurveyResultRepositoryStub } = makeSut()
+    // Dependency is returning a exception
+    jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId').mockImplementationOnce(throwError)
+
+    // Remove the await here
+    const promise = sut.load('any_survey_id')
+    // To catch the error here
+    await expect(promise).rejects.toThrow()
   })
 })
