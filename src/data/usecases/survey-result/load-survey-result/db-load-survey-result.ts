7a60e51f5ecf31ddff9473d560a5a0466432caaa
Thiago Andrade <thiago.adsix@gmail.com>
2022-05-08 16:52:05 -0300

    refactor: move LoadSurveyResult to a protocols file

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -1,6 +1,8 @@
-import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
-import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result'
-import { SurveyResultModel } from '@/domain/models/survey-result'
+import {
+  LoadSurveyResult,
+  LoadSurveyResultRepository,
+  SurveyResultModel
+} from './db-load-survey-result-protocols'
 
 export class DbLoadSurveyResult implements LoadSurveyResult {
   constructor (
